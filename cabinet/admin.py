79d87f2abbb0ee530e12047225ae2e866eae09eb
Maksym Bezhevets <begik4@gmail.com>
2023-07-19 19:11:57 +0300

    All models are registered

--- cabinet/admin.py
+++ cabinet/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from cabinet.models import Worker, Position, TaskType, Task
+
 
 # Register your models here.
+@admin.register(Worker)
+class WorkerAdmin(UserAdmin):
+    list_display = UserAdmin.list_display + ("position",)
+    fieldsets = UserAdmin.fieldsets + (
+        (("Additional info", {"fields": ("position",)}),)
+    )
+    add_fieldsets = UserAdmin.add_fieldsets + (
+        (
+            (
+                "Additional info",
+                {
+                    "fields": (
+                        "first_name",
+                        "last_name",
+                        "license_number",
+                    )
+                },
+            ),
+        )
+    )
+
+
+admin.site.register(Position)
+admin.site.register(TaskType)
+admin.site.register(Task)
--- cabinet/models.py
+++ cabinet/models.py
@@ -15,7 +15,10 @@ class Position(models.Model):
 
 
 class Worker(AbstractUser):
-    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+    position = models.ForeignKey(
+        Position,
+        on_delete=models.CASCADE,
+    )
 
     class Meta:
         verbose_name = "worker"
