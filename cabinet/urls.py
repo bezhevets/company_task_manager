05289575c9f915fe60513a1b1e9b8c2df9077c41
Maksym Bezhevets <begik4@gmail.com>
2023-07-19 22:29:34 +0300

    Created TaskCreateView

--- cabinet/urls.py
+++ cabinet/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from cabinet.views import TaskListView
+from cabinet.views import TaskListView, TaskCreateView
 
 urlpatterns = [
-    path("", TaskListView.as_view(), name="index"),
+    path("", TaskListView.as_view(), name="task-list"),
+    path("task/create/", TaskCreateView.as_view(), name="task-create"),
 ]
 
 
--- cabinet/views.py
+++ cabinet/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
 
 from cabinet.models import Task
@@ -9,6 +10,12 @@ from cabinet.models import Task
 
 class TaskListView(generic.ListView):
     model = Task
-    queryset = Task.objects.all().select_related("task_type").prefetch_related("workers")
-    template_name = "cabinet/index.html"
+    queryset = Task.objects.all().select_related("task_type").prefetch_related("assignees__workers")
+    template_name = "cabinet/task_list.html"
     context_object_name = "task_list"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("cabinet:task-list")
