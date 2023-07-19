3fd9183b841d36a5cd11f4585bd64369681530e8
Maksym Bezhevets <begik4@gmail.com>
2023-07-19 17:43:21 +0300

    Created models: Position, Worker, TaskType, Task

--- cabinet/models.py
+++ cabinet/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
+from django.urls import reverse
+from django.utils.timezone import now
+
 
 # Create your models here.
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = "worker"
+        verbose_name_plural = "workers"
+        ordering = ["username"]
+
+    def __str__(self) -> str:
+        return f"{self.username} (Full name:" \
+               f" {self.first_name} {self.last_name} " \
+               f"Position: {self.position})"
+
+    # def get_absolute_url(self) -> str:
+    #     return reverse("taxi:driver-detail", kwargs={"pk": self.pk})
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = (
+        ("Urgent", "Urgent"),
+        ("High", "High"),
+        ("Normal", "Normal"),
+        ("Low", "Low")
+    )
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateTimeField(default=now)
+    is_completed = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=PRIORITY_CHOICES,
+        default="Normal"
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(Worker, related_name="workers")
+
+    class Meta:
+        ordering = ["deadline"]
+
+    def __str__(self) -> str:
+        return self.name
